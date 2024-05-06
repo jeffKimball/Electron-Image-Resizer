673202b9183eae0e7c2987fd70204effb66501c7
J Kimball <jeffkimballis@gmail.com>
2024-05-06 16:49:00 -0500

    set up menu in main js min 29:26

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const path = require('path')
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 
 const isDev = process.env.NODE_ENV !== 'production'
 const isMac = process.platform === 'darwin'
 
+// Create the main window
 function createMainWindow(){
     const mainWindow = new BrowserWindow({
         title: 'Image Resizer',
@@ -19,18 +20,48 @@ function createMainWindow(){
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html'))
 }
 
+// App is ready
 app.whenReady().then(() => {
-    createMainWindow()
+    createMainWindow();
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-          createMainWindow()
+    // Implement menu
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createMainWindow();
+    }
+});
+
+
+
+const menu = [
+    ...(isMac ? [{
+        label: app.name,
+        submenu: [
+            {
+                label: 'About'
+            }
+        ]
+    }] : []),
+        {
+        label: 'File',
+        submenu: [
+
+            {
+                label: 'Quit',
+                click: () => app.quit(),
+            }
+    ]
         }
-      })
-})
+]
 
 app.on('window-all-closed', () => {
     if (!isMac) {
       app.quit()
     }
-  })
+})
+
+
